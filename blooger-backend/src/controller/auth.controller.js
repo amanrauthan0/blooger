5db54fa017577979e59dc5073d4fae8e172440d7
aman <aman44444490@gmail.com>
2026-08-16 20:24:12 +0530

    cokkie

--- blooger-backend/src/controller/auth.controller.js
+++ blooger-backend/src/controller/auth.controller.js
@@ -120,8 +120,8 @@ export async function login(req,res){
 
     res.cookie("refreshToken",refreshToken,{
         httpOnly:true,
-        secure:false,
-        sameSite:"strict",
+        secure: isProduction,
+        sameSite: isProduction ? "none" : "lax",
         maxAge:7*24*60*60*1000
     })
 
