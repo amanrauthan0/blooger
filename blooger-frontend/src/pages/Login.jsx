93f859f3a6e4a775f604b1740db34dfe18782014
aman <aman44444490@gmail.com>
2026-08-16 19:57:20 +0530

    api

--- blooger-frontend/src/pages/Login.jsx
+++ blooger-frontend/src/pages/Login.jsx
@@ -23,7 +23,7 @@ export default function Login() {
     e.preventDefault();
 
     try {
-      const res = await fetch(`${import.meta.env.VITE_API_URL}//api/auth/login`, {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
